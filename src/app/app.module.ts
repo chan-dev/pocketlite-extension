022818a092879759bb5b37b4a4ea4d3658a7622b
chan-dev <christian.m.austria@gmail.com>
2020-12-07 13:54:27 +0800

    feat: setup routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Route } from '@angular/router';
 
 import { AuthGuard } from './auth.guard';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+const routes: Route[] = [
+  {
+    path: '',
+    component: SavePageComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
+
 @NgModule({
+  /* prettier-ignore */
   declarations: [
     AppComponent,
     LoginComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
